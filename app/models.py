65d3924075205a5de75cf10a762820ff684c62ba
elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-09-22 21:18:54 -0300

    tirei choises para guardar no BD

--- app/models.py
+++ app/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
-CATEGORY = (
-  (1,'Eletrodomésticos'),
-  (2,'Eletronicos'),
-  (3,'Móveis'),
-)
+# CATEGORY = (
+#   (1,'Eletrodomésticos'),
+#   (2,'Eletronicos'),
+#   (3,'Móveis'),
+# )
 
-PRODUCT = (
-  (1,'Geladeira'),
-  (2,'Celular'),
-  (3,'Sofá'),
-)
+# PRODUCT = (
+#   (1,'Geladeira'),
+#   (2,'Celular'),
+#   (3,'Sofá'),
+# )
 
 
 
 class Stores(models.Model):
   cnpj = models.CharField(max_length=50)
   store_name = models.CharField(max_length=50)
-  product_category = models.CharField(
-    max_length=50,
-    choices=CATEGORY,
-    )
-  product = models.CharField(
-    max_length=100,
-    choices=PRODUCT,    
-    )
+  product_category = models.CharField(max_length=50)
+  product = models.CharField(max_length=100)
   
